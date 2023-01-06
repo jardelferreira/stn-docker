4e7341e39c474b0edec5753a79dd15b8b010231f
jardelferreira <jardelferreira.sousa@gmail.com>
2023-01-06 07:40:15 -0300

    set language Datatables

--- resources/views/publico/projects/index.blade.php
+++ resources/views/publico/projects/index.blade.php
@@ -6,39 +6,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Projectos</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
 
 </head>
 
 <body class="bg-light content">
-  <div class="content">
-    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
-        <a class="navbar-brand" href="#">SGLT</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="{{route('public.projects')}}">Publico <span class="sr-only">(current)</span></a>
-            </li>
-            @if (Auth::check())
-            <li class="nav-item">
-                <a class="nav-link" href="{{ url('/home') }}">Dashboard</a>
-            </li>
-                
-            @else
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}">Login</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('register') }}">Cadastro</a>
-            </li>
-            @endif                
+    <div class="content">
+        <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+            <a class="navbar-brand" href="#">SGLT</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-            {{-- <li class="nav-item dropdown">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ route('public.projects') }}">Publico <span
+                                class="sr-only">(current)</span></a>
+                    </li>
+                    @if (Auth::check())
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/home') }}">Dashboard</a>
+                        </li>
+                    @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">Cadastro</a>
+                        </li>
+                    @endif
+
+                    {{-- <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
                 Dropdown
               </a>
@@ -49,18 +51,18 @@
                 <a class="dropdown-item" href="#">Something else here</a>
               </div>
             </li> --}}
-          </ul>
-          {{-- <form class="form-inline my-2 my-lg-0">
+                </ul>
+                {{-- <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
           </form> --}}
-        </div>
-      </nav>
-  </div>
-<h1>Listagem de Projetos</h1>
-<hr>
+            </div>
+        </nav>
+    </div>
+    <h1>Listagem de Projetos</h1>
+    <hr>
     @if (count($projects))
-        <table class="table table-striped table-sm table-dark" id="projects" >
+        <table class="table table-striped table-sm table-dark" id="projects">
             <thead class="thead-inverse">
                 <tr>
                     <th>Projetos</th>
@@ -74,8 +76,10 @@
                         <td scope="row"><small>{{ $item->name }}</small></td>
                         {{-- <td scope="row">{{ $item->description }}</td> --}}
                         <td class="btn-group" role="group">
-                            <a class="btn btn-warning btn-sm mx-1" href="{{route('public.projects.bases',$item)}}"><small>Ver Bases</small></a>
-                            <a class="btn btn-primary btn-sm mx-1" href="{{route('public.projects.stoks',$item)}}"><small>Consultar Estoque</small></a>
+                            <a class="btn btn-warning btn-sm mx-1"
+                                href="{{ route('public.projects.bases', $item) }}"><small>Ver Bases</small></a>
+                            <a class="btn btn-primary btn-sm mx-1"
+                                href="{{ route('public.projects.stoks', $item) }}"><small>Consultar Estoque</small></a>
                             {{-- <a class="btn btn-info btn-sm mr-1" href="{{route('dashboard.projects.bases',['project' => $item->id])}}">Ver Bases</a> --}}
                             {{-- <a class="btn btn-warning btn-sm mr-1" href="{{route('dashboard.projects.stok',['project' => $item->id])}}">Ver Estoque</a> --}}
                         </td>
@@ -87,12 +91,32 @@
         <p>Não há Projetos para listagem</p>
     @endif
 </body>
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+{{-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+</script> --}}
+<script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous">
+</script>
 <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
 <script>
-    $(document).ready(function () {
-    $('#projects').DataTable();
-});
+    var lang = "";
+    
+    $(document).ready(function() {
+    $.ajax({
+        url: "//cdn.datatables.net/plug-ins/1.13.1/i18n/pt-BR.json",
+        success: function(result) {          
+            $('#projects').DataTable({
+                "language": result,
+                lengthMenu: [
+                            [10, 25, 50, -1],
+                            [10, 25, 50, 'Tudo'],
+                        ],
+            });
+        }
+    });
+
+    });
 </script>
+
 </html>
--- resources/views/publico/projects/stoks.blade.php
+++ resources/views/publico/projects/stoks.blade.php
@@ -5,47 +5,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Estoque do projeto - {{$project->name}}</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <title>Estoque do projeto - {{ $project->name }}</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
 
 </head>
 
 <body class="bg-light content">
-  <div class="content">
-    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
-        <a class="navbar-brand" href="#">SGLT</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="{{route('public.projects')}}">Publico <span class="sr-only">(current)</span></a>
-            </li>
-            @if (Auth::check())
-            <li class="nav-item">
-                <a class="nav-link" href="{{ url('/home') }}">Dashboard</a>
-            </li>
-                
-            @else
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}">Login</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('register') }}">Cadastro</a>
-            </li>
-            @endif                
-          </ul>
+    <div class="content">
+        <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+            <a class="navbar-brand" href="#">SGLT</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        </div>
-      </nav>
-  </div>
-<h1>Listagem de estoque do projeto - <small>{{$project->name}}</small></h1>
-<hr>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ route('public.projects') }}">Publico <span
+                                class="sr-only">(current)</span></a>
+                    </li>
+                    @if (Auth::check())
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/home') }}">Dashboard</a>
+                        </li>
+                    @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">Cadastro</a>
+                        </li>
+                    @endif
+                </ul>
+
+            </div>
+        </nav>
+    </div>
+    <h1>Listagem de estoque do projeto - <small>{{ $project->name }}</small></h1>
+    <hr>
     @if (count($project->bases()->get()))
-        <table class="table table-striped table-sm table-dark" id="stok" >
+        <table class="table table-striped table-sm table-dark" id="stok">
             <thead class="thead-inverse">
                 <tr>
                     <th>Base</th>
@@ -57,19 +59,17 @@
             </thead>
             <tbody>
                 @foreach ($project->bases()->get() as $base)
-                 @foreach ($base->sectors()->get() as $sector)
-                 @foreach ($sector->stoks()->get() as $stok)
-                     
-                 <tr>
-                     <td scope="row"><small>{{ $base->name }}</small></td>
-                     <td scope="row">{{ $sector->name }}</td>
-                     <td scope="row">{{ $stok->invoiceProduct->name }}</td>
-                     <td scope="row">{{ $stok->qtd}}</td>
-                     <td scope="row">{{ $stok->invoiceProduct->description}}</td>
-                    </tr>
-                    
+                    @foreach ($base->sectors()->get() as $sector)
+                        @foreach ($sector->stoks()->get() as $stok)
+                            <tr>
+                                <td scope="row"><small>{{ $base->name }}</small></td>
+                                <td scope="row">{{ $sector->name }}</td>
+                                <td scope="row">{{ $stok->invoiceProduct->name }}</td>
+                                <td scope="row">{{ $stok->qtd }}</td>
+                                <td scope="row">{{ $stok->invoiceProduct->description }}</td>
+                            </tr>
+                        @endforeach
                     @endforeach
-                 @endforeach
                 @endforeach
             </tbody>
         </table>
@@ -77,12 +77,34 @@
         <p>Não há Produtos para listagem</p>
     @endif
 </body>
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+{{-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> --}}
+<script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM="
+    crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous">
+</script>
 <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
 <script>
-    $(document).ready(function () {
-    $('#stok').DataTable();
-});
+    $(document).ready(function() {
+        var lang = "";
+
+        $(document).ready(function() {
+            $.ajax({
+                url: "//cdn.datatables.net/plug-ins/1.13.1/i18n/pt-BR.json",
+                success: function(result) {
+                    $('#stok').DataTable({
+                        "language": result,
+                        lengthMenu: [
+                            [10, 25, 50, -1],
+                            [10, 25, 50, 'Tudo'],
+                        ],
+                    });
+                }
+            });
+
+        });
+    });
 </script>
+
 </html>
