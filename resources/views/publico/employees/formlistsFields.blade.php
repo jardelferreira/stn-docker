6631d03911c0304b837774b937fc6cf549397220
jardelferreira <jardelferreira.sousa@gmail.com>
2023-01-16 11:57:13 -0300

    ajax with sweetalert pdf formlist

--- resources/views/publico/employees/formlistsFields.blade.php
+++ resources/views/publico/employees/formlistsFields.blade.php
@@ -1,10 +1,18 @@
 @extends('publico.page')
 
 @section('title', 'Formulário')
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+    integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
 @section('content')
-    <h2 class="bg-light"><a class="ms-4 btn btn-primary" onclick="window.print()" href="#">imprimir ficha<i class="fas fa-print fa-fw"></i></a></h2>
+    <h2 class="bg-light">
+        <a class="ms-4 btn btn-primary btn-sm" onclick="window.print()" href="#">imprimir ficha<i
+                class="fas fa-print fa-fw"></i></a>
+        <a id="del" class="btn btn-outline-danger btn-sm" href="#" role="button"
+            onclick="DownloadFile('{{ $formlist->name }}-{{ $employee->user->name }}.pdf','{{ route('formlistPdf', $formlist) }}')">
+            Salvar PDF - <i class="fa fa-file-pdf " aria-hidden="true"></i>
+        </a>
+    </h2>
     <div class="table-responsive text-nowrap bg-light mt-1 mx-1">
 
         <table class="table table-sm table-bordered" id="section-to-print">
@@ -69,13 +77,14 @@
                     </td>
                 </tr>
                 <tr>
-                    <td class="border border-dark text-center font-weight-bold" colspan="5"><span> Fornecimento</span></td>
+                    <td class="border border-dark text-center font-weight-bold" colspan="5"><span> Fornecimento</span>
+                    </td>
                     <td class="border border-dark text-center font-weight-bold" colspan="2"><span> Entrega</span></td>
                     <td class="border border-dark text-center font-weight-bold" colspan="2"><span> Devolução</span></td>
                 </tr>
             </tbody>
             <tbody id="list">
-            <tr id="head-itens" class="table table-bordered">
+                <tr id="head-itens" class="table table-bordered">
                     <th class="border border-dark text-center " width="25px"><span>#</span></th>
                     <th class="border border-dark text-center "><span>Código.</span></th>
                     <th class="border border-dark text-center "><span>Qtd.</span></th>
@@ -85,27 +94,28 @@
                     <th class="border border-dark text-center "><span>Assinatura</span></th>
                     <th class="border border-dark text-center "><span>Data</span></th>
                     <th class="border border-dark text-center "><span>Assinatura</span></th>
-            </tr>
-                    @foreach ($fields as $key => $field)
-                        <tr>
-                            <td class="border text-center border-dark p-0 m-0">{{$key +1}}</td>
-                            <td class="border text-center border-dark">{{ $field->stoks->id }}</td>
-                            <td class="border text-center border-dark">{{ $field->qtd_delivered }}</td>
-                            <td class="border text-center border-dark">{{ $field->stoks->invoiceProduct->ca_number }}</td>
-                            <td class="border text-center border-dark">{{ $field->stoks->invoiceProduct->description }}</td>
-                            <td class="border text-center border-dark">{{ date('d/m/Y', strtotime($field->date_delivered)) }}</td>
-                            <td class="border text-center border-dark">
-                                @if ($field->signature_delivered)
-                                    Assinado
-                                @else
-                                    Falha na assinatura
-                                @endif
-                            </td>
-                            <td class="border text-center border-dark">{{ $field->date_returned }}</td>
-                            <td class="border text-center border-dark">{{ $field->signature_returned }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
+                </tr>
+                @foreach ($fields as $key => $field)
+                    <tr>
+                        <td class="border text-center border-dark p-0 m-0"><p style="padding: 0; margin: 0; font-size: 0.8em;">{{ $key + 1 }}</p></td>
+                        <td class="border text-center border-dark"><p style="padding: 0; margin: 0; font-size: 0.8em;">{{ $field->stoks->id }}</p></td>
+                        <td class="border text-center border-dark"><p style="padding: 0; margin: 0; font-size: 0.8em;">{{ $field->qtd_delivered }}</p></td>
+                        <td class="border text-center border-dark"><p style="padding: 0; margin: 0; font-size: 0.8em;">{{ $field->stoks->invoiceProduct->ca_number }}</p></td>
+                        <td class="border text-center border-dark"><p style="padding: 0; margin: 0; font-size: 0.8em;">{{ $field->stoks->invoiceProduct->description }}</p></td>
+                        <td class="border text-center border-dark"><p style="padding: 0; margin: 0; font-size: 0.8em;">{{ date('d/m/Y', strtotime($field->date_delivered)) }}</p>
+                        </td>
+                        <td class="border text-center border-dark"><p style="padding: 0; margin: 0; font-size: 0.8em;">
+                            @if ($field->signature_delivered)
+                                Assinado
+                            @else
+                                Falha na assinatura
+                            @endif
+                        </p></td>
+                        <td class="border text-center border-dark"><p style="padding: 0; margin: 0; font-size: 0.8em;">{{ $field->date_returned }}</p></td>
+                        <td class="border text-center border-dark"><p style="padding: 0; margin: 0; font-size: 0.8em;">{{ $field->signature_returned }}</p></td>
+                    </tr>
+                @endforeach
+            </tbody>
         </table>
     </div>
 @endsection
@@ -156,8 +166,10 @@
 @endsection
 
 @section('js')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js" integrity="sha512-BNaRQnYJYiPSqHHDb58B0yaPfCu+Wgds8Gp/gU33kqBtgNS4tSPHuGibyoeqMV/TJlSKda6FXzoEyYGjTe+vXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"
+        integrity="sha512-BNaRQnYJYiPSqHHDb58B0yaPfCu+Wgds8Gp/gU33kqBtgNS4tSPHuGibyoeqMV/TJlSKda6FXzoEyYGjTe+vXA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script>
         $("input:checkbox").on('click', function() {
             // in the handler, 'this' refers to the box clicked on
@@ -174,5 +186,49 @@
                 $box.prop("checked", false);
             }
         });
+
+        function DownloadFile(fileName, url) {
+            $.ajax({
+                url: url,
+                cache: false,
+                xhr: function() {
+                    var xhr = new XMLHttpRequest();
+                    xhr.onreadystatechange = function() {
+                        if (xhr.readyState == 2) {
+                            if (xhr.status == 200) {
+                                xhr.responseType = "blob";
+                                console.log("recebido")
+                            } else {
+                                xhr.responseType = "text";
+                            }
+                        }
+                    };
+                    console.log("enviando")
+                    return xhr;
+                },
+                success: function(data) {
+                    //Convert the Byte Data to BLOB object.
+                    console.log("convertendo para blob")
+                    var blob = new Blob([data], {
+                        type: "application/octetstream"
+                    });
+                    //Check the Browser type and download the File.
+                    var isIE = false || !!document.documentMode;
+                    if (isIE) {
+                        window.navigator.msSaveBlob(blob, fileName);
+                    } else {
+                        console.log("preparando download")
+                        var url = window.URL || window.webkitURL;
+                        link = url.createObjectURL(blob);
+                        var a = $("<a />");
+                        a.attr("download", fileName);
+                        a.attr("href", link);
+                        $("body").append(a);
+                        a[0].click();
+                        $("body").remove(a);
+                    }
+                }
+            });
+        };
     </script>
 @endsection
--- resources/views/publico/page.blade.php
+++ resources/views/publico/page.blade.php
@@ -121,6 +121,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM="
         crossorigin="anonymous"></script>
+@section('plugins.Sweetalert2', true)
     @yield('js')
     <script>
         $(document).ready(() => {
