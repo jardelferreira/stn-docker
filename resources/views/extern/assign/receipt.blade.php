dce179bedddf9f6b9486ab3cb1fa0587d6c23f07
jardelferreira <jardelferreira.sousa@gmail.com>
2023-05-02 04:48:25 -0300

    fix size screen

--- resources/views/extern/assign/receipt.blade.php
+++ resources/views/extern/assign/receipt.blade.php
@@ -229,7 +229,7 @@
                 background: "linear-gradient( 95.2deg, rgba(173,252,234,1) 26.8%, rgba(192,229,246,1) 64% )",
                 showCancelButton: false,
                 showConfirmButton: false,
-                width: 1024,
+                width: 660,
 
             })
 
