37301ab68309970646a8d7a2db5dca35e977523b
jardelferreira <jardelferreira.sousa@gmail.com>
2023-05-15 16:25:22 -0300

    middleware public active

--- config/adminlte.php
+++ config/adminlte.php
@@ -247,7 +247,7 @@ return [
             'icon' => 'fa fa-dashcube',
             'icon_color'  => 'primary',
             'classes' => "bg-light",
-            'role' => ["public_projects"]
+            'can' => ["public_projects"]
         ],
         [
             'text' => 'DP',
