a88ab3f6f3f9fb5a8cc50b104ec03bf0002f90e6
jardelferreira <jardelferreira.sousa@gmail.com>
2023-07-14 07:23:48 -0300

    fix modal replacement

--- resources/views/dashboard/projects/bases/employees/formlistsFields.blade.php
+++ resources/views/dashboard/projects/bases/employees/formlistsFields.blade.php
@@ -549,7 +549,7 @@
             } = await Swal.fire({
                 title: "informe as senhas para prosseguir!",
                 showCancelButton: true,
-                html: `<div class="row">
+                html: `<div class="row container">
                             <div class="form-group col-10">
                             <label for="user_pass">Sua senha:</label>
                             <input type="password"
