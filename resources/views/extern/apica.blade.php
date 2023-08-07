a35068b5ff773958b46e59b927878036f24af4f8
jardelferreira <jardelferreira.sousa@gmail.com>
2023-08-07 13:18:40 -0300

    fix apica page

--- resources/views/extern/apica.blade.php
+++ resources/views/extern/apica.blade.php
@@ -23,8 +23,8 @@
                 <label for="caNumber">Número do CA:</label>
                 <input type="number" class="form-control" id="caNumber" placeholder="Digite o número do CA">
             </div>
-            <button style="height: 50%; position: relative; top: 30px;" type="button" class="col-4 btn btn-primary"
-                id="btnBuscarCA">Buscar CA</button>
+            <button style="height: 50%; position: relative; top: 30px;" type="button"
+                class="col-4 btn btn-primary" id="btnBuscarCA">Buscar CA</button>
             <!-- Botão para enviar a requisição -->
         </div>
         <div id="error"></div>
@@ -71,91 +71,83 @@
         integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
     </script>
     -->
-
-    {{-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> --}}
-    <script>
-        if (window.mobileAndTabletCheck()) {
-            document.write(
-                    `<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-mobile/1.4.5/jquery.mobile.min.js" integrity="sha512-FbnNPigZZCDhMM22ezvan3xN2/E/oBd9GxqtpiBcK1VZuzBAt0V1EmTtteoYuYxWrqwqBA6zb1OTB887QSjAsw==" crossorigin="anonymous" referrerpolicy="no-referrer">
-    </script>`)
-    }else{
-    document.write(`
     <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM="
-        crossorigin="anonymous"></script>`)
-    }
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        window.mobileAndTabletCheck = function() {
+            let check = false;
+            (function(a) {
+                if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i
+                    .test(a) ||
+                    /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i
+                    .test(a.substr(0, 4))) check = true;
+            })(navigator.userAgent || navigator.vendor || window.opera);
+            return check;
+        };
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-mobile/1.4.5/jquery.mobile.min.js" 
+        integrity="sha512-FbnNPigZZCDhMM22ezvan3xN2/E/oBd9GxqtpiBcK1VZuzBAt0V1EmTtteoYuYxWrqwqBA6zb1OTB887QSjAsw==" 
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        if (window.mobileAndTabletCheck()) {
+            source = document.createElement('script');
+            source.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery-mobile/1.4.5/jquery.mobile.min.js"
+            source.integrity="sha512-FbnNPigZZCDhMM22ezvan3xN2/E/oBd9GxqtpiBcK1VZuzBAt0V1EmTtteoYuYxWrqwqBA6zb1OTB887QSjAsw=="
+            source.crossorigin="anonymous"
+            source.referrerpolicy="no-referrer"
+        }else{
+            source = document.createElement('script');
+            source.src="https://code.jquery.com/jquery-3.7.0.js"
+            source.integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM="
+            source.crossorigin="anonymous"
+        }
 
-    window.mobileAndTabletCheck = function() {
-    let check = false;
-    (function(a) {
-    if
-    (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge
-    |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm(
-    os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows
-    ce|xda|xiino|android|ipad|playbook|silk/i
-    .test(a) ||
-    /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a
-    wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s
-    )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1
-    u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-|
-    |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac(
-    |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt
-    |kwc\-|kyo(c|k)|le(no|xi)|lg(
-    g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-|
-    |o|v)|zz)|mt(50|p1|v
-    )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v
-    )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-|
-    )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i
-    .test(a.substr(0, 4))) check = true;
-    })(navigator.userAgent || navigator.vendor || window.opera);
-    return check;
-    };
+        document.body.append(source)
 
+        $("#btnBuscarCA").on('click', () => {
+            var caNumber = $("#caNumber").val();
 
-    $("#btnBuscarCA").on('click', () => {
-    $.ajaxSetup({
-    headers: {
-    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    }
-    });
-    var caNumber = $("#caNumber").val();
-
-    $.ajax({
-    url: `https://www.jfwebsystem.com.br/stn/apica/${caNumber}`,
-    method: "GET",
-    cache: false,
-    success: function(data) {
-    //Convert the Byte Data to BLOB object.
-    $("#registroCA").text(data.RegistroCA);
-    $("#dataValidade").text(data.DataValidade);
-    $("#situacao").text(data.Situacao);
-    $("#nrProcesso").text(data.NRProcesso);
-    $("#cnpj").text(data.CNPJ);
-    $("#razaoSocial").text(data.RazaoSocial);
-    $("#natureza").text(data.Natureza);
-    $("#nomeEquipamento").text(data.NomeEquipamento);
-    $("#descricaoEquipamento").text(data.DescricaoEquipamento);
-    $("#marcaCA").text(data.MarcaCA);
-    $("#referencia").text(data.Referencia);
-    $("#cor").text(data.Cor || "Nulo");
-    $("#aprovadoParaLaudo").text(data.AprovadoParaLaudo);
-    $("#restricaoLaudo").text(data.RestricaoLaudo || "Nulo");
-    $("#observacaoAnaliseLaudo").text(data.ObservacaoAnaliseLaudo);
-    $("#cnpjLaboratorio").text(data.CNPJLaboratorio);
-    $("#razaoSocialLaboratorio").text(data.RazaoSocialLaboratorio);
-    $("#nrLaudo").text(data.NRLaudo || "Nulo");
-    $("#norma").text(data.Norma || "Nulo");
-    },
-    error: function(error) {
-    // Lidar com o erro, se necessário
-    $("#error").append(`<p>${error}</p>`)
-    $("#error").append(`<p>${error.Message}</p>`)
-    $("#error").append(`<p>${error.messge}</p>`)
-    $("#error").append(`<p>${error.status}</p>`)
-    $("#error").append(`<p>${JSON.parse(error)}</p>`)
-    $("#error").append(`<p>${JSON.stringfy(error)}</p>`)
-    }
-    });
-    });
+            $.ajax({
+                url: `https://www.jfwebsystem.com.br/stn/apica/${caNumber}`,
+                method: "GET",
+                cache: false,
+                success: function(data) {
+                    //Convert the Byte Data to BLOB object.
+                    $("#registroCA").text(data.RegistroCA);
+                    $("#dataValidade").text(data.DataValidade);
+                    $("#situacao").text(data.Situacao);
+                    $("#nrProcesso").text(data.NRProcesso);
+                    $("#cnpj").text(data.CNPJ);
+                    $("#razaoSocial").text(data.RazaoSocial);
+                    $("#natureza").text(data.Natureza);
+                    $("#nomeEquipamento").text(data.NomeEquipamento);
+                    $("#descricaoEquipamento").text(data.DescricaoEquipamento);
+                    $("#marcaCA").text(data.MarcaCA);
+                    $("#referencia").text(data.Referencia);
+                    $("#cor").text(data.Cor || "Nulo");
+                    $("#aprovadoParaLaudo").text(data.AprovadoParaLaudo);
+                    $("#restricaoLaudo").text(data.RestricaoLaudo || "Nulo");
+                    $("#observacaoAnaliseLaudo").text(data.ObservacaoAnaliseLaudo);
+                    $("#cnpjLaboratorio").text(data.CNPJLaboratorio);
+                    $("#razaoSocialLaboratorio").text(data.RazaoSocialLaboratorio);
+                    $("#nrLaudo").text(data.NRLaudo || "Nulo");
+                    $("#norma").text(data.Norma || "Nulo");
+                },
+                error: function(error) {
+                    // Lidar com o erro, se necessário
+                    $("#error").append(`<p>${error}</p>`)
+                    $("#error").append(`<p>${error.Message}</p>`)
+                    $("#error").append(`<p>${error.messge}</p>`)
+                    $("#error").append(`<p>${error.status}</p>`)
+                    $("#error").append(`<p>${JSON.parse(error)}</p>`)
+                    $("#error").append(`<p>${JSON.stringfy(error)}</p>`)
+                }
+            });
+        });
     </script>
 </body>
 
