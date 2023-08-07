32133fa2bacf554e6d55e5a27491d471c49796f7
jardelferreira <jardelferreira.sousa@gmail.com>
2023-08-06 23:47:00 -0300

    apica page

--- resources/views/extern/apica.blade.php
+++ resources/views/extern/apica.blade.php
@@ -93,7 +93,7 @@
         $("#btnBuscarCA").on('click', () => {
             var caNumber = $("#caNumber").val();
             $.ajax({
-                url: `https://www.jfwebsystem.com.br/CA/${caNumber}`,
+                url: `${window.location.href}/${caNumber}`,
                 method: "GET",
                 cache: false,
                 success: function(data) {
