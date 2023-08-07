666ce50adb05791eee2e197e8d0f974dd2b015c8
jardelferreira <jardelferreira.sousa@gmail.com>
2023-08-06 22:15:55 -0300

    apica page

--- resources/views/extern/apica.blade.php
+++ resources/views/extern/apica.blade.php
@@ -1,145 +1,188 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="pt-br">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Consulta CA</title>
-    <!-- Adicione o link do CSS do Bootstrap -->
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Minhas Fichas</title>
 </head>
 
 <body>
-    <div class="container mt-1">
-        <h4>Informações do Certificado de Aprovação</h4>
-        <div class="row">
-            <!-- Input para receber o número do CA -->
-            <div class="form-group col-6">
-                <label for="caNumber">Número do CA:</label>
-                <input type="number" class="form-control" id="caNumber" placeholder="Digite o número do CA">
-            </div>
-            <button style="height: 50%; position: relative; top: 30px;" type="button" class="col-3 btn btn-primary"
-                id="btnBuscarCA">Buscar CA</button>
-            <!-- Botão para enviar a requisição -->
-        </div>
-       <div id="error"></div>
-        <div class="row mt-4">
-            <div class="col-md-6">
-                <!-- Campos para exibir as informações -->
-                <p><strong>Registro CA:</strong> <span id="registroCA"></span></p>
-                <p><strong>Data de Validade:</strong> <span id="dataValidade"></span></p>
-                <p><strong>Situação:</strong> <span id="situacao"></span></p>
-                <p><strong>Número do Processo:</strong> <span id="nrProcesso"></span></p>
-                <p><strong>CNPJ:</strong> <span id="cnpj"></span></p>
-                <p><strong>Razão Social:</strong> <span id="razaoSocial"></span></p>
-                <p><strong>Natureza:</strong> <span id="natureza"></span></p>
-                <p><strong>Nome do Equipamento:</strong> <span id="nomeEquipamento"></span></p>
-                <p><strong>Descrição do Equipamento:</strong> <span id="descricaoEquipamento"></span></p>
-                <p><strong>Marca do CA:</strong> <span id="marcaCA"></span></p>
-                <p><strong>Referência:</strong> <span id="referencia"></span></p>
-            </div>
-            <div class="col-md-6">
-                <p><strong>Cor:</strong> <span id="cor">Nulo</span></p>
-                <p><strong>Aprovado para Laudo:</strong> <span id="aprovadoParaLaudo"></span></p>
-                <p><strong>Restrição de Laudo:</strong> <span id="restricaoLaudo">Nulo</span></p>
-                <p><strong>Observação de Análise de Laudo:</strong> <span id="observacaoAnaliseLaudo"></span></p>
-                <p><strong>CNPJ do Laboratório:</strong> <span id="cnpjLaboratorio"></span></p>
-                <p><strong>Razão Social do Laboratório:</strong> <span id="razaoSocialLaboratorio"></span></p>
-                <p><strong>NR do Laudo:</strong> <span id="nrLaudo">Nulo</span></p>
-                <p><strong>Norma:</strong> <span id="norma">Nulo</span></p>
-            </div>
+    <h1>{{ $user->name }}</h1>
+    <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+            @foreach ($base_formlists as $base)
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapse{{ $loop->index }}" aria-expanded="true"
+                        aria-controls="collapse{{ $loop->index }}">
+                        {{ $base->name }}
+
+                    </button>
+                </h2>
+                <div id="collapse{{ $loop->index }}" class="accordion-collapse collapse" aria-labelledby="headingOne"
+                    data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">FICHA</th>
+                                    <th scope="col">PDF</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($base->formlistsByEmlpoyee as $formlist)
+                                    <tr>
+                                        <td scope="row">{{ $loop->index + 1 }}</td>
+                                        <td>{{ $formlist->name }}</td>
+                                        <td>
+                                            <a id="del" class="btn ml-1 rounded btn-outline-danger btn-sm"
+                                                href="#" role="button"
+                                                onclick="DownloadFile('{{ $formlist->name }}-{{ $user->name }}.pdf','{{ route('stn.formlistPdf', $formlist->pivot->formlist_id) }}')">
+                                                Salvar PDF - <i class="fa fa-file-pdf " aria-hidden="true"></i>
+                                            </a>
+                                            {{-- <a class="ml-1 rounded btn btn-primary btn-sm" onclick="window.print()"
+                                                href="#">imprimir ficha<i class="fas fa-print fa-fw"></i></a> --}}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
-    <!-- Adicione o link do JavaScript do Bootstrap e do jQuery (opcional) -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"
-        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
-        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
     </script>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <!-- Script para fazer a requisição AJAX -->
+    -->
+    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM="
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
-        $(document).ready(function() {
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        function DownloadFile(fileName, url) {
+            $(document).ready(() => {
+
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+                window.mobileAndTabletCheck = function() {
+                    let check = false;
+                    (function(a) {
+                        if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i
+                            .test(a) ||
+                            /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i
+                            .test(a.substr(0, 4))) check = true;
+                    })(navigator.userAgent || navigator.vendor || window.opera);
+                    return check;
+                };
+            });
+
+            var popupTextElement = "";
+            $.ajax({
+                url: url,
+                cache: false,
+                xhr: function() {
+                    var xhr = new XMLHttpRequest();
+                    xhr.onreadystatechange = function() {
+                        if (xhr.readyState == 2) {
+                            if (xhr.status == 200) {
+                                xhr.responseType = "blob";
+                                popupTextElement.innerHTML = popupTextElement.innerHTML +
+                                    `<br/>
+                                        <div class="alert alert-success" role="alert">
+                                        Processando...
+                                        </div>`
+                            } else {
+                                xhr.responseType = "text";
+                            }
+                        }
+                    };
+
+                    swal.fire({
+                        title: "<span id='save-popup-title'>Gerando PDF para ficha...</span>",
+                        html: "<div id='save-popup-icon'></div><span id='save-popup-message'></span>",
+                        confirmButtonColor: "#1a7bb9",
+                        confirmButtonText: "Ok",
+                        allowOutsideClick: false,
+                        didOpen: () => {
+                            Swal.showLoading()
+                            popupTextElement = document.getElementById(
+                                "save-popup-message");
+                            popupTextElement.innerHTML = popupTextElement.innerHTML +
+                                `<br/>
+                                        <div class="alert alert-success" role="alert">
+                                        Enviando...
+                                        </div>`;
+                        }
+                    });
+                    return xhr;
+                },
+                success: function(data) {
+                    //Convert the Byte Data to BLOB object.
+                    popupTextElement.innerHTML = popupTextElement.innerHTML +
+                        `<br/>
+                            <div class="alert alert-success" role="alert">
+                            Gerando pdf...
+                            </div>`
+
+                    var blob = mobileAndTabletCheck() ? new Blob([data], {
+                        type: 'application/pdf'
+                    }) : new Blob([data], {
+                        type: "application/octetstream"
+                    });
+                    //Check the Browser type and download the File.
+                    var isIE = false || !!document.documentMode;
+                    if (isIE) {
+                        window.navigator.msSaveBlob(blob, fileName);
+                    } else {
+                        popupTextElement.innerHTML = popupTextElement.innerHTML +
+                            `<br/>
+                                        <div class="alert alert-success" role="alert">
+                                        Preparando seu download...
+                                        </div>`
+                        var url = window.URL || window.webkitURL;
+                        link = url.createObjectURL(blob);
+                        var a = $("<a />");
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Tudo pronto, baixando!',
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+                        a.attr("download", fileName);
+                        a.attr("href", link);
+                        $("body").append(a);
+                        a[0].click();
+                        $("body").remove(a);
+                    }
                 }
             });
-            $("#btnBuscarCA").click(function() {
-                // Obter o valor do input
-                var caNumber = $("#caNumber").val();
-                $.ajax({
-                    method: "GET",
-                    url: `https://apica.jfwebsystem.com.br/CA/${caNumber}`,
-                }).done(function(data) {
-                        // Preencher os campos com os valores recebidos
-                        $("#registroCA").text(data.RegistroCA);
-                        $("#dataValidade").text(data.DataValidade);
-                        $("#situacao").text(data.Situacao);
-                        $("#nrProcesso").text(data.NRProcesso);
-                        $("#cnpj").text(data.CNPJ);
-                        $("#razaoSocial").text(data.RazaoSocial);
-                        $("#natureza").text(data.Natureza);
-                        $("#nomeEquipamento").text(data.NomeEquipamento);
-                        $("#descricaoEquipamento").text(data.DescricaoEquipamento);
-                        $("#marcaCA").text(data.MarcaCA);
-                        $("#referencia").text(data.Referencia);
-                        $("#cor").text(data.Cor || "Nulo");
-                        $("#aprovadoParaLaudo").text(data.AprovadoParaLaudo);
-                        $("#restricaoLaudo").text(data.RestricaoLaudo || "Nulo");
-                        $("#observacaoAnaliseLaudo").text(data.ObservacaoAnaliseLaudo);
-                        $("#cnpjLaboratorio").text(data.CNPJLaboratorio);
-                        $("#razaoSocialLaboratorio").text(data.RazaoSocialLaboratorio);
-                        $("#nrLaudo").text(data.NRLaudo || "Nulo");
-                        $("#norma").text(data.Norma || "Nulo");
-                    }).fail(function(jqXHR, textStatus) {
-                        $('#error').append(` <div class="alert alert-danger" role="alert">
-                            <h4 class="alert-heading">Erro!</h4>
-                            <p>${textStatus}</p>
-                            <p class="mb-0">desculfa-pulse</p>
-                            </div>`)
-                    });
-                // Fazer a requisição AJAX
-                // $.ajax({
-                //     type: "GET",
-                //     url: `https://apica.jfwebsystem.com.br/CA/${caNumber}`, // Substitua pela URL da sua API
-                //     success: function(data) {
-                //         // Preencher os campos com os valores recebidos
-                //         $("#registroCA").text(data.RegistroCA);
-                //         $("#dataValidade").text(data.DataValidade);
-                //         $("#situacao").text(data.Situacao);
-                //         $("#nrProcesso").text(data.NRProcesso);
-                //         $("#cnpj").text(data.CNPJ);
-                //         $("#razaoSocial").text(data.RazaoSocial);
-                //         $("#natureza").text(data.Natureza);
-                //         $("#nomeEquipamento").text(data.NomeEquipamento);
-                //         $("#descricaoEquipamento").text(data.DescricaoEquipamento);
-                //         $("#marcaCA").text(data.MarcaCA);
-                //         $("#referencia").text(data.Referencia);
-                //         $("#cor").text(data.Cor || "Nulo");
-                //         $("#aprovadoParaLaudo").text(data.AprovadoParaLaudo);
-                //         $("#restricaoLaudo").text(data.RestricaoLaudo || "Nulo");
-                //         $("#observacaoAnaliseLaudo").text(data.ObservacaoAnaliseLaudo);
-                //         $("#cnpjLaboratorio").text(data.CNPJLaboratorio);
-                //         $("#razaoSocialLaboratorio").text(data.RazaoSocialLaboratorio);
-                //         $("#nrLaudo").text(data.NRLaudo || "Nulo");
-                //         $("#norma").text(data.Norma || "Nulo");
-                //     },
-                //     error: function(error) {
-                //         // Lidar com o erro, se necessário
-                //         alert("Erro ao buscar o CA.");
-                //     }
-                // });
-            });
-        });
+        };
     </script>
 </body>
 
