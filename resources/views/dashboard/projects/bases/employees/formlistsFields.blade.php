4963e26788dfb9e618a9d1636a1ac06965f9b084
jardelferreira <jardelferreira.sousa@gmial.com>
2024-01-27 14:27:49 -0300

    adjustments from forlistsPdf page

--- resources/views/dashboard/projects/bases/employees/formlistsFields.blade.php
+++ resources/views/dashboard/projects/bases/employees/formlistsFields.blade.php
@@ -244,7 +244,7 @@
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     </div>
-    <div class="card page-break">
+    {{-- <div class="card page-break">
         <div class="card-header d-flex">
             <h5 class="mb-0">Número CA: <span data-item="numero_ca"></span></h5>
             <div class="ml-3"> <a href="#" target="_blank" class="text-light font-weight-bold"
@@ -312,7 +312,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 @endsection
 
 @section('js')
