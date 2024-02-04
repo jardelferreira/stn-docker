c6fbd6f5d42463c1b89b744349e469d8e9bb94f8
jardelferreira <jardelferreira.sousa@gmial.com>
2024-02-04 16:53:20 -0300

    user_projects

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -31,7 +31,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        // dd(User::with('employee')->get()[0]);
+        dd(User::with('employee.formlists')->get());
         // dd(User::all()[0]->employee()->first());
         return \view('dashboard.users.index', [
             'users' => User::with('employee')->get(),
