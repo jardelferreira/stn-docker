628b788f04aa23f0b3885c805604494cb47636af
jardelferreira <jardelferreira.sousa@gmial.com>
2024-04-26 21:10:59 -0300

    fix add products

--- app/Http/Controllers/StoksController.php
+++ app/Http/Controllers/StoksController.php
@@ -45,7 +45,7 @@ class StoksController extends Controller
         // dd($sector->project->providers()->get());
         $stok = Stoks::where('id',735)->with(['sector','base','project','invoiceProduct.provider','product'])->first();
 
-        dd($stok->invoiceProduct->provider,$stok, InvoiceProducts::find(1318)->first());
+        dd($stok->invoiceProduct->provider,$stok, InvoiceProducts::where('id',1318)->first());
 
         return view('dashboard.projects.bases.sectors.stoks.create',[
             'sector' => $sector
